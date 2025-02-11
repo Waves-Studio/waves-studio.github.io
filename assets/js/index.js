7e219e4dc49df71b7ce35381fe49fe03b8060ff7
thefrenchaxo <thefrenchaxo@gmail.com>
2025-02-11 14:10:25 +0100

    Added the animation and started responsive design.
    On branch main
    
    Changes to be committed:
        modified:   assets/css/home-style.css
    	modified:   assets/icon/arrow.svg
    	renamed:    assets/js/script.js -> assets/js/index.js
    	modified:   index.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     const topBar = document.getElementById("top-bar");
     const arrow = document.getElementById("arrow");
+    const mainTitle = document.querySelector(".main-title");
 
     window.addEventListener("scroll", function () {
         if (window.scrollY > 50) { 
             topBar.classList.add("show");
             arrow.classList.add("hide");
+            mainTitle.classList.add("small-title");
         } else {
-            topBar.classList.add("show");
+            topBar.classList.add("show"); // On garde la top bar visible
         }
     });
 });
+
